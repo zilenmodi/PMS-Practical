40d668c82e1e0c4f8715056f9ce7cb56e887e38f
zilenmodi <zilen.m@simformsolutions.com>
2023-03-30 14:17:13 +0530

    add readme file and fix small bugs like delete press button

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,45 +1,51 @@
-
+// Retrieve product information from local storage
 let products = JSON.parse(localStorage.getItem('productCards'))
+
+// Get the product ID from the URL query string
 let currentID = window.location.search.split('=')[1]
+
+// Filter the products array to find the product with the matching ID
 let currentProduct = products.filter((product) => product._id == currentID)
 
-console.log(currentProduct);
-
+// If the product ID is found, display the product information on the page
 if (currentID !== undefined && currentProduct.length !== 0) {
-    document.getElementById('content-view').innerHTML = `<div class="container ">
-    <div class="card text-center mt-3 w-sm-25 mx-auto">
-        <div class="card-header">
-            PID: ${currentProduct[0]._id}
-        </div>
-        <div class="card-body">
-            <div class="content-view-imagebox">
-                <img src="${currentProduct[0].image}">
+    document.getElementById('content-view').innerHTML = `
+    <div class="container ">
+        <div class="card text-center mt-3 w-sm-25 mx-auto">
+            <div class="card-header">
+                PID: ${currentProduct[0]._id}
             </div>
-            <h5 class="card-title mt-3">${currentProduct[0].productName}
-            </h5>
-            <p class="card-text">${currentProduct[0].description}
-            </p>
-            <div class="price text-success mt-2">
-                <h5>$${currentProduct[0].price}</h5>
+            <div class="card-body">
+                <div class="content-view-imagebox">
+                    <img src="${currentProduct[0].image}">
+                </div>
+                <h5 class="card-title mt-3">${currentProduct[0].productName}
+                </h5>
+                <p class="card-text">${currentProduct[0].description}
+                </p>
+                <div class="price text-success mt-2">
+                    <h5>$${currentProduct[0].price}</h5>
+                </div>
+                <a href="../../index.html" class="btn btn-primary mt-2">Go Back</a>
             </div>
-            <a href="../../index.html" class="btn btn-primary mt-2">Go Back</a>
         </div>
-    </div>
     </div>`
 }
+// If the product ID is not found, display a 404 error message on the page
 else {
-    document.getElementById('content-view').innerHTML = `<div class="container ">
-    <div class="card text-center mt-3 w-sm-25 mx-auto">
-        <div class="card-header">
-            PID: Not Available
+    document.getElementById('content-view').innerHTML = `
+    <div class="container ">
+        <div class="card text-center mt-3 w-sm-25 mx-auto">
+            <div class="card-header">
+                PID: Not Available
+            </div>
+            <div class="card-body">
+                <h3 class="card-title mt-3">Page Not Found!
+                </h3>
+                <p class="card-text">Error 404
+                </p>
+                <a href="./index.html" class="btn btn-primary mt-2">Go Back</a>
+            </div>
         </div>
-        <div class="card-body">
-            <h3 class="card-title mt-3">Page Not Found!
-            </h3>
-            <p class="card-text">Error 404
-            </p>
-            <a href="./index.html" class="btn btn-primary mt-2">Go Back</a>
-        </div>
-    </div>
     </div>`
 }
